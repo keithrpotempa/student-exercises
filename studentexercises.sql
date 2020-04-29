620f55de72082895c498c7f110cb1854b9d3ca7d
keithrpotempa <keithrpotempa@gmail.com>
2020-04-29 15:00:04 -0500

    finished bk2-ch3 exercises

--- studentexercises.sql
+++ studentexercises.sql
@@ -37,3 +37,56 @@ CREATE TABLE StudentExercises (
 	FOREIGN KEY(ExerciseId) REFERENCES Exercise(Id),
 	FOREIGN KEY(StudentId) REFERENCES Student(Id)
 )
+
+INSERT INTO Cohort (Name)
+VALUES 
+	("C38"),
+	("C37"),
+	("C36");
+
+INSERT INTO Exercise (Name, Lang)
+VALUES 
+	("Student Exercises", "Python"),
+	("Floral Arrangements", "Python"),
+	("Coins to Cash", "Python"),
+	("Daily Journal", "JavaScript"),
+	("Kennel", "React");
+
+SELECT * FROM Instructor;
+
+INSERT INTO Instructor (FirstName, LastName, Slack, Specialty, CohortId)
+VALUES
+	("Jisie", "David", "jisiedavid", "Python", 1),
+	("Andy", "Collins", "andycollins", "Python", 2),
+	("Kristen", "Norris", "kristennorris", "C#", 3);
+
+INSERT INTO Student (FirstName, LastName, Slack, CohortId)
+VALUES
+	("Dar", "Poeta", "darpoeta", 1),
+	("Alice", "Quota", "alicequota", 2),
+	("Ricky", "Ness", "rickyness", 3),
+	("Tess", "Arven", "tessarven", 1),
+	("Mickey", "Doff", "mickeydoff", 2),
+	("Dag", "Degger", "dagdegger", 3),
+	("Stuck", "Here", "stuckhere", 1);
+	
+SELECT * FROM StudentExercises;
+
+INSERT INTO StudentExercises (ExerciseId, StudentId)
+VALUES
+	(1, 1),
+	(2, 1),
+	(1, 2),
+	(2, 2),
+	(1, 3),
+	(2, 3),
+	(3, 3),
+	(1, 4),
+	(2, 4),
+	(3, 4),
+	(1, 5),
+	(2, 5),
+	(1, 6),
+	(2, 6),
+	(1, 7),
+	(2, 7);
