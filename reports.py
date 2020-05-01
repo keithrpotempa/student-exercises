91de08810a9720c294e28075a7c3ebacdf0f8db1
keithrpotempa <keithrpotempa@gmail.com>
2020-05-01 11:20:10 -0500

    refactoring complete

--- reports.py
+++ reports.py
@@ -11,22 +11,27 @@ class StudentExerciseReports():
     def __init__(self):
         self.db_path = "/home/kpotempa/workspace/student-exercises/studentexercises.db"
 
-    def report(self, row_factory, query):
-        """ Generic report method"""
+    def get_data(self, row_factory, query):
+        """ Generic get data method"""
         
         with sqlite3.connect(self.db_path) as conn:
-            conn.row_factory = row_factory
+            # This handles situations where we don't need a row_factory
+            if row_factory != None:
+                conn.row_factory = row_factory
             db_cursor = conn.cursor()
             db_cursor.execute(query)
             response = db_cursor.fetchall()
             
-        return response
-            
-            # if len(response) > 0:
-            #   for row in response:
-            #       print(f"  - {row}")
-            # else:
-            #   print("   - None")
+            return response
+
+    def print_report(self, report_title, data):
+        print(f"{report_title}")
+        if len(data) > 0:
+            for row in data:
+                print(f"  - {row}")
+        else:
+            print("   - None")
+        print("")
 
     def all_students(self):
         """Retrieve all students with the cohort name"""
@@ -47,32 +52,24 @@ class StudentExerciseReports():
             ORDER BY s.CohortId
             """
             
-        print(f"All Students:")
-        response = self.report(row_factory, query)
-        if len(response) > 0:
-            for row in response:
-                print(f"  - {row}")
-        else:
-            print("   - None")
-        print("")
+        response = self.get_data(row_factory, query)
+        self.print_report("All Students", response)
                 
     def all_cohorts(self):
         """Retrieve all cohorts"""
         row_factory = lambda cursor, row: Cohort(row[1])
         query = """SELECT * FROM Cohort;"""
         
-        print(f"All Cohorts:")
-        self.report(row_factory, query)
-        print("")
+        response = self.get_data(row_factory, query)
+        self.print_report("All Cohorts", response)
         
     def all_exercises(self):
         """Retrieve all exercises"""
         row_factory = lambda cursor, row: Exercise(row[1], row[2])
         query = """SELECT * FROM Exercise;"""
         
-        print(f"All Exercises:")
-        self.report(row_factory, query)
-        print("")
+        response = self.get_data(row_factory, query)
+        self.print_report("All Exercises", response)
         
     def all_instructors(self):
         """Retrieve all instructors with the cohort name"""
@@ -92,9 +89,8 @@ class StudentExerciseReports():
             JOIN Cohort c ON i.CohortId = c.Id
             """
             
-        print(f"All Instructors:")
-        self.report(row_factory, query)
-        print("")
+        response = self.get_data(row_factory, query)
+        self.print_report("All Instructors", response)
         
     # FIXME: this method doesn't seem quite right to me, 
     # considering we've already all the exercise data over to python instances
@@ -107,19 +103,31 @@ class StudentExerciseReports():
           WHERE Lang = "{language}";
         """
         
-        print(f"Exercises in {language} include:")
-        self.report(row_factory, query)
-        print("")
-        
+        response = self.get_data(row_factory, query)
+        self.print_report(f"Exercises in {language} include", response)
+
     def students_per_exercise(self):
         """Retrieve all exercises and the students working on each"""
-        # Different enough from other reports as to not utilize the report method
-        exercises = dict()
-
-        with sqlite3.connect(self.db_path) as conn:
-            db_cursor = conn.cursor()
+        def print_results (dataset):
+            exercises = dict()
+            
+            for row in dataset:
+                exercise_id = row[0]
+                exercise_name = row[1]
+                student_id = row[2]
+                student_name = f'{row[3]} {row[4]}'
+                
+                if exercise_name not in exercises:
+                    exercises[exercise_name] = [student_name]
+                else:
+                    exercises[exercise_name].append(student_name)
+                    
+            for exercise_name, students in exercises.items():
+                print(exercise_name)
+                for student in students:
+                    print(f'\t- {student}')
         
-        db_cursor.execute("""
+        query = f"""
                 select
             e.Id ExerciseId,
             e.Name,
@@ -129,33 +137,21 @@ class StudentExerciseReports():
         from Exercise e
         join StudentExercise se on se.ExerciseId = e.Id
         join Student s on s.Id = se.StudentId
-        """)
+        """
         
-        dataset = db_cursor.fetchall()
+        # There is no row_factory for this:
+        # we're not making any instances here
+        row_factory = None
         
-        for row in dataset:
-            exercise_id = row[0]
-            exercise_name = row[1]
-            student_id = row[2]
-            student_name = f'{row[3]} {row[4]}'
-            
-            if exercise_name not in exercises:
-                exercises[exercise_name] = [student_name]
-            else:
-                exercises[exercise_name].append(student_name)
-                
-        for exercise_name, students in exercises.items():
-            print(exercise_name)
-            for student in students:
-                print(f'\t- {student}')
-        
+        response = self.get_data(row_factory, query)
+        print_results(response)
 
 reports = StudentExerciseReports()
-reports.all_students()
+# reports.all_students()
 # reports.all_cohorts()
 # reports.all_exercises()
 # reports.exercises_from_language("JavaScript")
 # reports.exercises_from_language("Python")
 # reports.exercises_from_language("C#")
 # reports.all_instructors()
-# reports.students_per_exercise()
+reports.students_per_exercise()
